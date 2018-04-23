f4b549b24a1a40e8e9b639b38701b92297c80207
cchav92 <38115467+cchav92@users.noreply.github.com>
2018-04-23 20:36:38 -0300

    Add files via upload

---
+++ 01-Adicion/suma de dos enteros.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+main(){
+       int a, b, f;
+       
+       cout<<"ingrese el primer numero: "<<endl;
+       cin>>a;
+       cout<<"ingrese el segundo numero: "<<endl;
+       cin>>b;
+       cout<<"\nla suma es: "<<a+b<<endl;
+       cout<<"\ntoque cualquier tecla para finalizar"<<endl;
+       cin>>f;
+       return 0;
+       }
